bd65c94e96f5e4e96b4473b5443c4f12388ea972
t08mac23 <t08mac23@gmail.com>
2023-04-18 19:06:43 +0900

    ダウンロード

---
+++ 5-9_class$instance/lesson2.php
@@ -0,0 +1,30 @@
+<?php
+// 下記のようにlesson1のファイルをインポートしてください。
+// (他のファイルを参照する関数はいくつかあるので調べてください。)
+// (lesson1の文字列がそのまま出力されてしまっていても大丈夫とします。)
+
+
+// 下記のクラスを作成してください。
+
+// Patient 
+// lesson1からPersonを継承
+
+// height: float 身長（ｍ）
+// weight: float 体重 (kg)
+
+// __construct(name: string, age:int, gender: string, height: float, weight: float)
+// 名前、年齢、性別、身長、体重を受け取り初期化する。
+
+// calculateStandardWeight() :float
+// 平均体重を返す （身長 × 身長 × 22)
+
+// getHeight()
+// 身長を返す
+
+// getWeight()
+// 体重を返す
+
+// クラスが完成したら適当なインスタンスを生成し、
+// それぞれの関数を使用して下記のフォーマットで出力してください。
+
+// 〇〇さんの身長は〇〇mなので平均体重は〇〇kgです。 現在の体重は〇〇kgです。
