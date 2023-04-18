62b66cbb02f3693445f2099120b08cb2eba4b73d
t08mac23 <t08mac23@gmail.com>
2023-04-18 17:07:40 +0900

    lesson4完了

--- 5-8_function/lesson4.php
+++ 5-8_function/lesson4.php
@@ -23,6 +23,25 @@ $product = 150; // 商品金額
 
 function calc($yen, $product) {
     // この関数内に処理を記述
+    $change = $yen - $product;
+    $money = [ 10000, 5000, 1000, 500, 100, 50, 10, 1]; {
+        if ($yen < $product) {
+            echo ($yen - $product) * -1 ."円足りません。";
+            return;
+        }
+        foreach ($money as $i => $j) {
+            $i = floor($change / $j);
+            $change -= $j * $i;
+            if (999 < $j ) {
+                echo $j."円札x".$i."枚"."、";
+            }elseif (1 < $j && $j < 1000) {
+                echo $j."円玉x".$i."枚"."、";
+            }else {
+                echo $j."円玉x".$i."枚";
+            }
+        }
+    }
+    // return $yen, $product;
 }
 ?>
 <!DOCTYPE html>
@@ -34,6 +53,9 @@ function calc($yen, $product) {
 <body>
     <section>
         <!-- ここに結果表示 -->
+        <?php
+         echo calc($yen, $product);
+        ?>
     </section>
 </body>
 </html>
