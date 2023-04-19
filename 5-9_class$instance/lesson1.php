ba6683f1d0ee637358c7dd90a8e5709e3a881a8e
t08mac23 <t08mac23@gmail.com>
2023-04-19 18:52:24 +0900

    lesson1完成

--- 5-9_class$instance/lesson1.php
+++ 5-9_class$instance/lesson1.php
@@ -18,3 +18,34 @@
 // クラスができたら適当なインスタンスを作成し、
 // 自己紹介→年齢加算→自己紹介の順にメソッドを動かして年齢を確認してください。
 
+class Person {
+  public string $name;
+
+  public int $age;
+
+  public string $gender;
+
+  public function __construct ($name, $age, $gender) {
+    $this->name = $name;
+    $this->age = $age;
+    $this->gender = $gender;
+  }
+
+  public function selfIntroduction (): string {
+    return '私の名前は'.$this->name.'です。年齢は'.$this->age.'歳です。性別は'.$this->gender.'です。';
+  }
+
+  public function addAge (): void {
+    echo '誕生日が来ました。';
+    $this->age = $this->age + 1;
+  }
+}
+
+$yamada = new Person ('山田', 23, '男性');
+
+echo $yamada->selfIntroduction ();
+echo "<br>";
+echo $yamada->addAge ();
+echo "<br>";
+echo $yamada->selfIntroduction ();
+?>
